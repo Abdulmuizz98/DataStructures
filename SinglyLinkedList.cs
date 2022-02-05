7e9e0b4799a56d7c9428f60f098b6bdf07f31031
Abdulmuizz98 <hamzatabdulmuizz@outlook.com>
2022-02-05 11:20:30 +0100

    Completed render of SinglyLinkedList Abstract Data Type

--- SinglyLinkedList.cs
+++ SinglyLinkedList.cs
@@ -94,15 +94,129 @@ namespace DataStructures
             
             }
         }
-        /*
-            clear
-            remove
-            removefirst
-            removelast
-            removeat
-            contains
-            indexof
-            tostring
-        */
+        public T RemoveFirst()
+        {
+            if (IsEmpty()) throw new Exception("Empty List!");
+
+            T data = Head.Data;
+            Head = Head.Next;
+
+            size--;
+            if(IsEmpty()) Tail = default(Node<T>);
+
+            return data;              
+        } 
+        public T RemoveLast()
+        {
+            if (IsEmpty()) throw new Exception("Empty List!");
+            if(size == 1 ) return RemoveFirst();
+            else{
+                T data = Tail.Data;
+                Node<T> trav = Head;
+                
+                for(int i=0; i < size; i++, trav = trav.Next)
+                {
+                    if(trav.Next.Equals(Tail))
+                    {
+                        Tail = trav;
+                        Tail.Next = default(Node<T>);
+                        break;
+                    }
+                }
+                size--;
+                return data;              
+            }
+        }
+        private T Remove(Node<T> node) 
+        {
+            if (node.Next == default(Node<T>)) return RemoveLast();
+            if (node == Head) return RemoveFirst();
+
+            T data = node.Data;
+            Node<T> trav = Head;
+                
+            for(int i=0; i < size; i++, trav = trav.Next)
+            {
+                if(trav.Next.Equals(node))
+                {
+                    trav.Next = node.Next;
+                    
+                    break;
+                }
+            }
+            --size;
+
+            //clean up
+            node = node.Next = default(Node<T>); 
+            return data;
+        }
+        public T RemoveAt(int index)
+        {
+            if (index < 0 || index >= size) throw new Exception("Out of range!");
+            
+            int i  = 0;
+            Node<T> trav;
+            
+            for(i = 0, trav = Head; i != index; i++ )
+            {
+                trav = trav.Next;
+            }
+            return Remove(trav);
+        }
+        public void Clear()
+        {
+            Node<T> trav = Head;
+
+            while (trav != default(Node<T>))
+            {
+                Node<T> next = trav.Next;
+                trav.Next = default(Node<T>);
+                trav = next;
+            }
+            Head = Tail = default(Node<T>);
+            size = 0;
+        }
+        public bool Contains(Object obj)
+        {
+            return IndexOf(obj) != -1;
+        }
+        public int IndexOf(Object obj)
+        {
+            int i;
+            Node<T> trav;
+            if(obj ==  default(Node<T>))
+            {
+                for(i=0,trav = Head; trav != default(Node<T>); i++, trav = trav.Next)
+                {
+                    if(trav.Data.Equals(default(T)))
+                    {
+                        Remove(trav);
+                        return i;
+                    }
+                }
+            }else
+            {
+                for(i=0,trav = Head; trav != default(Node<T>); i++, trav = trav.Next)
+                {
+                    if(trav.Data.Equals(obj))
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+            Node<T> trav = Head;
+            while(trav != default(Node<T>))
+            {
+                sb.Append(trav.Data + ",");      
+            }
+            sb.Append("]");
+            return sb.ToString();
+        }
     }
 }
